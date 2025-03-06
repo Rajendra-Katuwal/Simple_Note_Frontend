a60cf2e00f5981fd3a263464c4be63278b885428
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-03-06 17:39:37 +0545

    Added the registration Page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -7,7 +7,7 @@ const LoginPage = () => {
 
   return (
     <div className='h-screen w-screen flex justify-center items-center bg-slate-10'>
-      <form method="post" className='flex flex-col min-w-2xs max-w-sm px-8 py-5 rounded-xl bg-slate-200 gap-2'>
+      <form className='flex flex-col min-w-2xs max-w-sm px-8 py-5 rounded-xl bg-slate-200 gap-2'>
         <h2 className='text-center font-bold text-lg'>Login</h2>
         <InputField type="text" name="username" id="username" />
         <InputField type={isChecked ? "text" : "password"} name="password" id="password" />
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import InputField from '../components/InputField'
+import MyButton from '../components/MyButton'
+import CheckBox from '../components/CheckBox'
 const RegisterPage = () => {
+  const [isChecked, setIsChecked] = useState(false);
+
   return (
-    <form method="post">
-        <h2>Register</h2>
-        <input type="text" name="username" id="username" />
-        <input type="email" name="email" id="email" />
-        <input type="password" name="password" id="password" />
-        {/* <input type="password" name="password" id="password" /> */}
-        <button type='submit'>Register</button>
-    </form>
-  )
+    <div className='h-screen w-screen flex justify-center items-center bg-slate-10'>
+      <form className='flex flex-col min-w-2xs max-w-sm px-8 py-5 rounded-xl bg-slate-200 gap-2 border-2 border-black'>
+        <h2 className='text-center font-bold text-lg'>Registration</h2>
+        <InputField type="text" name="username" id="username" placeholder='Username'/>
+        <InputField type="email" name="email" id="email" placeholder='Email'/>
+        <InputField type={isChecked ? "text" : "password"} name="password" id="password" placeholder='Password'/>
+        <InputField type={isChecked ? "text" : "password"} name="re-password" id="re-password" placeholder='Confirm Password'/>
+        <CheckBox label="Show Password" className="mt-2" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
+        <MyButton buttonText={"Register"} />
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPage;
